b51db07ee1a01d8933b3542d138181fcc8aa1512
Luiz Evangelista <luizhenrique.se@gmail.com>
2023-09-09 00:25:45 -0300

    fix: upgrading flapdoodle mongo test lib

--- src/test/kotlin/io/github/luizimcpi/IntegrationTestUtils.kt
+++ src/test/kotlin/io/github/luizimcpi/IntegrationTestUtils.kt
@@ -3,15 +3,15 @@ package io.github.luizimcpi
 import de.flapdoodle.embed.mongo.MongodExecutable
 import de.flapdoodle.embed.mongo.MongodProcess
 import de.flapdoodle.embed.mongo.MongodStarter
-import de.flapdoodle.embed.mongo.config.MongodConfigBuilder
+import de.flapdoodle.embed.mongo.config.MongodConfig
 import de.flapdoodle.embed.mongo.config.Net
 import de.flapdoodle.embed.mongo.distribution.Version
 import de.flapdoodle.embed.process.runtime.Network
 
 fun startMongo(host: String, port: Int): MongodProcess {
     val starter = MongodStarter.getDefaultInstance()
-    val mongodConfig = MongodConfigBuilder()
-        .version(Version.Main.V4_0)
+    val mongodConfig: MongodConfig = MongodConfig.builder()
+        .version(Version.Main.V5_0)
         .net(Net(host, port, Network.localhostIsIPv6()))
         .build()
     val mongodExecutable: MongodExecutable = starter.prepare(mongodConfig)
